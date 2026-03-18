91a1927199f711dcadac9cc1e105fb34fe304161
otsuguak <cesar@lumengroup.com.co>
2026-03-18 15:22:30 -0500

    Add files via upload

--- app-crm.js
+++ app-crm.js
@@ -1,9 +1,6 @@
 // --- IMPORTACIÓN DE SUPABASE ---
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
 
-const supabaseUrl = 'https://rqjfaztnaktizrgllhna.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJxamZhenRuYWt0aXpyZ2xsaG5hIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzIxMzk1NjksImV4cCI6MjA4NzcxNTU2OX0.cb6LSWq5YZ7BKRdBx2VoeD-m1gUonfpU_MJemaTSB3U';
-
 // 1. ALMACENAMIENTO SEGURO
 const RAM = {};
 const almacenamientoSeguro = {
